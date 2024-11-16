ee78daaa0638c20191a5f6e10a7d08377b1c166d
Christophe <c.soule@outlook.fr>
2024-11-16 18:29:22 +0100

    Add files via upload

--- TestExercice1.py
+++ TestExercice1.py
@@ -1,40 +1,254 @@
 import unittest
+from turtle import pd
 
-from exercice1 import compute_period, compute_auto_correlation
+from exercice1 import compute_period, compute_auto_correlation, correlation_plot, compute_period_improved_generator
 
 
 class MyTestCase(unittest.TestCase):
-#     def test_exercice1(self):
-#         k = 899  # faire varier k
-#         l = 0  # faire varier l
-#         m = 32768
-#         x_0 = 12  # faire varier x_0
-#
-# #        k_values = [600, 899, ]
-#
-#         for x_0 in range(1, 20, 1):
-#             compute_period(k, l, m, x_0)
 
-    def test_exercice1(self):
-        # k = pow(7, 5)  # faire varier k
-        #
-        # l = 0  # faire varier l
-        # m = 6075 #pow(2, 31) - 1
-        # x_0 = 12  # faire varier x_0
-
-        # k = pow(7, 5) # faire varier k
-        #
-        # l = 0  # faire varier l
-        # m = pow(2, 31) - 1
-        # x_0 = 12  # faire varier x_0
-
-        k = 899  # faire varier k
-
-        l = 0  # faire varier l
+    #compléter pour générer les tableaux
+    def test_exercice_1_period_p1(self):
+        k = 899
+        l = 0
         m = 32768
-        x_0 = 12  # faire varier x_0
+        x_0 = 12
+
+        print('periode : ' + str(compute_period(k, l, m, x_0)))
+
+        print('k variation test :')
+        for k_var in range(895, 905):
+            print(k_var, compute_period(k_var, l, m, x_0))
+
+        print('l variation test :')
+        for l_var in range(0, 10):
+            print(l_var, compute_period(k, l_var, m, x_0))
+
+        print('m variation test :')
+        for m_var in range(32764, 32774):
+            print(m_var, compute_period(k, l, m_var, x_0))
+
+        print('xo variation test :')
+        for x_0_var in range(8, 18):
+            print(x_0_var, compute_period(k, l, m, x_0_var))
+
+
+    def test_exercice_1_period_p2(self):
+        k = 16807
+        l = 0
+        m = 6075
+        x_0 = 12
+        results = []
+
+        # Liste des valeurs pour k
+        k_values = range(16802, 16812)
+        print('k variation test :')
+        for k_var in range(16802, 16812):
+            print(k_var, compute_period(k_var, l, m, x_0))
+
+        # Générer les résultats pour chaque valeur de k
+        results = [(k_var, compute_period(k_var, l, m, x_0)) for k_var in k_values]
+
+        # Créer la structure du tableau LaTeX
+        latex_table = "\\begin{tabular}{|c|c|} \\hline\n"
+        latex_table += "$k$ & $\\text{Période}$ \\\\ \\hline\n"  # En-tête du tableau
+
+        # Ajouter les lignes du tableau avec les valeurs de k et les résultats
+        for k, period in results:
+            latex_table += f"${k}$ & ${period}$ \\\\ \\hline\n"
+
+        latex_table += "\\end{tabular}"
+
+        # Sauvegarder dans un fichier LaTeX
+        with open("tableau_k_periode.tex", "w") as file:
+            file.write(latex_table)
+
+        print("Tableau LaTeX généré dans 'tableau_k_periode.tex'")
+
+
+    def test_exercice_1_period_p3(self):
+        k = pow(7, 5)
+        l = 0
+        m = pow(2, 31) - 1
+        x_0 = 12
+        print('periode : ' + str(compute_period(k, l, m, x_0)))
+
+    def test_exercice_2_correlation_plot_p1(self):
+        k = 899
+        l = 0
+        m = 32768
+        x_0 = 12
+        correlation_plot(k, l, m, x_0)
+
+        print('k variation test :')
+        for k_var in range(895, 905):
+            correlation_plot(k_var, l, m, x_0)
+
+        print('l variation test :')
+        for l_var in range(0, 10):
+            correlation_plot(k, l_var, m, x_0)
+
+        print('m variation test :')
+        for m_var in range(32764, 32774):
+            correlation_plot(k, l, m_var, x_0)
+
+        print('xo variation test :')
+        for x_0_var in range(8, 18):
+            correlation_plot(k, l, m, x_0_var)
+
+
+    def test_exercice_2_correlation_plot_p2(self):
+        k = 16807
+        l = 0
+        m = 6075
+        x_0 = 12
+        correlation_plot(k, l, m, x_0)
+
+        print('k variation test :')
+        for k_var in range(16802, 16812):
+            correlation_plot(k_var, l, m, x_0)
+
+        print('l variation test :')
+        for l_var in range(0, 10):
+            correlation_plot(k, l_var, m, x_0)
+
+        print('m variation test :')
+        for m_var in range(6070, 6080):
+            correlation_plot(k, l, m_var, x_0)
+
+        print('xo variation test :')
+        for x_0_var in range(8, 18):
+            correlation_plot(k, l, m, x_0_var)
+
+
+    def test_exercice_2_correlation_plot_p3(self):
+        k = pow(7, 5)
+        l = 0
+        m = pow(2, 31) - 1
+        x_0 = 12
+        correlation_plot(k, l, m, x_0)
+
+
+    def test_exerice_3_autocorrelation_p1(self):
+        k = 899
+        l = 0
+        m = 32768
+        x_0 = 12
 
         compute_period(k, l, m, x_0)
-    #    tau = 10
-     #   correlation = compute_auto_correlation(k, l, m, x_0, tau)
-        #print(correlation)
+        N = [10000, 100000]
+        tau = [10, 100, 1000]
+
+        # Calcul des corrélations
+        correlations = []
+        for n in N:
+            row = []
+            for t in tau:
+                row.append(compute_auto_correlation(k, l, m, x_0, n, t))
+            correlations.append(row)
+
+        # Génération du tableau LaTeX
+        latex_table = "\\begin{tabular}{|c|" + "c|" * len(tau) + "} \\hline\n"
+        latex_table += "$n \\backslash \\tau$ & " + " & ".join([f"${t}$" for t in tau]) + " \\\\ \\hline\n"
+
+        for i, n in enumerate(N):
+            latex_table += f"${n}$ & " + " & ".join([f"{corr:.5e}" for corr in correlations[i]]) + " \\\\ \\hline\n"
+
+        latex_table += "\\end{tabular}"
+
+        # Sauvegarde dans un fichier LaTeX
+        with open("tableau_double_entree.tex", "w") as file:
+            file.write(latex_table)
+
+        print("Tableau LaTeX généré dans 'tableau_double_entree.tex'")
+
+
+    def test_exerice_3_autocorrelation_p2(self):
+        k = 16807
+        l = 0
+        m = 6075
+        x_0 = 12
+
+        compute_period(k, l, m, x_0)
+        N = [10000, 100000]
+        tau = [10, 100, 1000]
+
+        # Calcul des corrélations
+        correlations = []
+        for n in N:
+            row = []
+            for t in tau:
+                row.append(compute_auto_correlation(k, l, m, x_0, n, t))
+            correlations.append(row)
+
+        # Génération du tableau LaTeX
+        latex_table = "\\begin{tabular}{|c|" + "c|" * len(tau) + "} \\hline\n"
+        latex_table += "$n \\backslash \\tau$ & " + " & ".join([f"${t}$" for t in tau]) + " \\\\ \\hline\n"
+
+        for i, n in enumerate(N):
+            latex_table += f"${n}$ & " + " & ".join([f"{corr:.5e}" for corr in correlations[i]]) + " \\\\ \\hline\n"
+
+        latex_table += "\\end{tabular}"
+
+        # Sauvegarde dans un fichier LaTeX
+        with open("tableau_double_entree.tex", "w") as file:
+            file.write(latex_table)
+
+        print("Tableau LaTeX généré dans 'tableau_double_entree.tex'")
+
+
+    def test_exerice_3_autocorrelation_p3(self):
+        k = pow(7, 5)
+        l = 0
+        m = pow(2, 31) - 1
+        x_0 = 12
+
+        N = [10000, 100000]
+        tau = [10, 100, 1000]
+
+        # Calcul des corrélations
+        correlations = []
+        for n in N:
+            row = []
+            for t in tau:
+                row.append(compute_auto_correlation(k, l, m, x_0, n, t))
+            correlations.append(row)
+
+        # Génération du tableau LaTeX
+        latex_table = "\\begin{tabular}{|c|" + "c|" * len(tau) + "} \\hline\n"
+        latex_table += "$n \\backslash \\tau$ & " + " & ".join([f"${t}$" for t in tau]) + " \\\\ \\hline\n"
+
+        for i, n in enumerate(N):
+            latex_table += f"${n}$ & " + " & ".join([f"{corr:.5e}" for corr in correlations[i]]) + " \\\\ \\hline\n"
+
+        latex_table += "\\end{tabular}"
+
+        # Sauvegarde dans un fichier LaTeX
+        with open("tableau_double_entree.tex", "w") as file:
+            file.write(latex_table)
+
+        print("Tableau LaTeX généré dans 'tableau_double_entree.tex'")
+
+
+    def test_exerice_4_period_improved_generator(self):
+
+        # k = 16807
+        # l = 0
+        # m = 6075
+        # x_0 = 12
+
+        k = 16807
+        l = 0
+        m = 6075
+        x_0 = 12
+
+        # k = 899
+        # l = 1
+        # m = 32768
+        # x_0 = 12
+
+        # k = pow(7, 5)
+        # l = 0
+        # m = pow(2, 31) - 1
+        # x_0 = 12
+        tau = 100
+        print(compute_period_improved_generator(k, l, m, x_0,tau))
--- exercice1.py
+++ exercice1.py
@@ -1,35 +1,45 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 def compute_period(k, l, m, x_0):
+    x = {x_0}  # création tableau x initialisé à x0
+    period_result = 1  # initialisation de période
 
+    x_n = x_0  # initialisation x_n
+    x_n_plus_1 = (k * x_n + l) % m
+
+    while not (x_n_plus_1 in x):  # tant que xn+1 différent des xn du tableau, continuer
+        x.add(x_n_plus_1)
+        x_n = x_n_plus_1
+        x_n_plus_1 = (k * x_n + l) % m
+        period_result += 1
+        if period_result % 10000 == 0:
+            print('current period : ' + str(period_result))
+
+    return period_result
+
+def correlation_plot(k, l, m, x_0):
+    period_max_for_print = 100000
     x_plot = []
     y_plot = []
 
-    x = [x_0]  # création tableau x initialisé à x0
-
-
-    period_result = 1  # initialisation période = 0
+    period_result = 1  # initialisation de période
 
     x_n = x_0  # initialisation x_n
     x_n_plus_1 = (k * x_n + l) % m
 
     x_plot.append(x_n)
     y_plot.append(x_n_plus_1)
-    while not (x_n_plus_1 in x):  # tant que xn+1 différent des xn du tableau, continuer
-
-        x.append(x_n_plus_1)
+    while period_result < period_max_for_print:
         x_n = x_n_plus_1
         x_n_plus_1 = (k * x_n + l) % m
         x_plot.append(x_n)
         y_plot.append(x_n_plus_1)
         period_result += 1
-        if (period_result == 2048) :
-            break
 
+    print('period : ' + str(period_result))
     print(len(x_plot))
-    plt.scatter(x_plot, y_plot, label='energy')
+    plt.scatter(x_plot, y_plot, label='correlation', s=1)
     plt.xlabel('xn')
     plt.ylabel('xn+1')
     plt.legend()
@@ -37,20 +47,12 @@ def compute_period(k, l, m, x_0):
     plt.show()
 
 
-    print('period : ' + str(period_result))
-
-
-def compute_auto_correlation(k, l, m, x_0, tau):
-
+def compute_auto_correlation(k, l, m, x_0, N, tau):
     x = [x_0]  # création tableau x initialisé à x0
-    N = 10000
-    N_moyenne = N - tau
-
-
     x_n = x_0  # initialisation x_n
     x_n_plus_1 = (k * x_n + l) % m
 
-    for i in range(1 ,N,1) :
+    for i in range(1, N + tau, 1):
         x.append(x_n_plus_1)
         x_n = x_n_plus_1
         x_n_plus_1 = (k * x_n + l) % m
@@ -59,13 +61,74 @@ def compute_auto_correlation(k, l, m, x_0, tau):
     correlation_denominator = 0
     x_mean = np.mean(x)
 
-
-    for i in range(1 ,N_moyenne,1):
+    for i in range(1, N, 1):
         correlation_numerator += (x[i] - x_mean) * (x[i + tau] - x_mean)
         correlation_denominator += pow((x[i] - x_mean), 2)
 
     correlation = correlation_numerator / correlation_denominator
     return correlation
-    #print(correlation)
 
 
+def compute_period_improved_generator(k, l, m, x_0, tau):
+    k_1 = 899
+    l_1 = 0
+    m_1 = 32768
+    x_0_1 = 12
+
+    m1_size = 800
+    m1 = []
+    N = 100000  # nombre d'éléments pour calculer la corrélation
+    N_moyenne = N - tau-1
+
+    x_n_1 = x_0_1
+    x_n_plus_1_1 = (k_1 * x_n_1 + l_1) % m_1
+
+    for i in range(0, m1_size):
+        m1.append(x_n_plus_1_1)
+        x_n_1 = x_n_plus_1_1
+        x_n_plus_1_1 = (k_1 * x_n_1 + l_1) % m_1
+
+    # on calcule l'indice avec le 2ème générateur
+    x_n = x_0
+    x_n_plus_1 = (k * x_n + l) % m
+    index = int(x_n_plus_1 * m1_size / m)
+    x_n = x_n_plus_1
+
+    x_n_plus_1_improved = m1[index]
+
+    #on remplace par un nombre tiré avec le premier générateur
+    x_n_plus_1_1 = (k_1 * x_n_1 + l_1) % m_1
+    m1[index] = x_n_plus_1_1
+    x_n_1 = x_n_plus_1_1
+
+    x = []  # création tableau x initialisé à x0
+    period_result = 1  # initialisation de période
+    # générateur amélioré
+
+
+    for i in range(1, N, 1): #tant que xn+1 différent des xn du tableau, continuer
+        x.append(x_n_plus_1_improved)
+
+        x_n_plus_1 = (k * x_n + l) % m
+        index = int(x_n_plus_1 * m1_size / m)
+        x_n = x_n_plus_1
+
+        x_n_plus_1_improved = m1[index]
+
+        x_n_plus_1_1 = (k_1 * x_n_1 + l_1) % m_1
+        m1[index] = x_n_plus_1_1
+        x_n_1 = x_n_plus_1_1
+
+        period_result += 1
+       # print(x_n_plus_1_improved)
+
+    correlation_numerator = 0
+    correlation_denominator = 0
+    x_mean = np.mean(x)
+
+    for i in range(1, N_moyenne, 1):
+        correlation_numerator += (x[i] - x_mean) * (x[i + tau] - x_mean)
+        correlation_denominator += pow((x[i] - x_mean), 2)
+
+    correlation = correlation_numerator / correlation_denominator
+    return correlation
